ce26055aa5beecedd6ab221a2ad8a9ae720d9584
Badgers <brianbadger93@gmail.com>
2023-04-18 12:44:28 -0500

    added fetch for friends of character

--- assets/script.js
+++ assets/script.js
@@ -13,11 +13,11 @@ let divContainer = document.querySelector("#divContainer")
 generateButton.addEventListener('click' , function(){
     generateButton.classList.add('hide')
 
+    // Fetches character's name, age, and gender.
     fetch("https://randomuser.me/api/")
     .then(response => response.json())
     .then(results => {
         console.log(results)
-        
         
         let userGender = document.createElement('h2')
         userGender.textContent = results.results[0].gender
@@ -38,27 +38,47 @@ generateButton.addEventListener('click' , function(){
         
     })
 
-
-    
-
-    
+    // Fetches three friends for the character
     fetch("https://www.swapi.tech/api/people?page=1&limit=100")
     .then(response => response.json())
-    .then(data => console.log(data))
-    
+    .then(people => {
+        console.log(people);
+        let randomIndex1 = Math.floor(Math.random() * people.results.length);
+        let randomIndex2 = Math.floor(Math.random() * people.results.length);
+        let randomIndex3 = Math.floor(Math.random() * people.results.length);
+        
+        let friend1 = document.createElement('h2');
+        friend1.textContent = people.results[randomIndex1].name;
+        divContainer.appendChild(friend1);
+        console.log(friend1);
+
+        let friend2 = document.createElement('h2');
+        friend2.textContent = people.results[randomIndex2].name;
+        divContainer.appendChild(friend2);
+        console.log(friend2);
+
+        let friend3 = document.createElement('h2');
+        friend3.textContent = people.results[randomIndex3].name;
+        divContainer.appendChild(friend3);
+        console.log(friend3);
+    })
+        
+    // Fetches what planet the character is from
     fetch("https://www.swapi.tech/api/planets?page=1&limit=100")
     .then(response => response.json())
     .then(data => console.log(data))
     
+    // Fetches what species the character is
     fetch("https://www.swapi.tech/api/species?page=1&limit=100")
     .then(response => response.json())
     .then(data => console.log(data))
     
+    // If the character is older than 16, they drive a starship, if not, they do not have a starship yet
     fetch("https://www.swapi.tech/api/starships?page=1&limit=100")
     .then(response => response.json())
     .then(data => console.log(data))
     
-    
+    // If the character is older than 16, they drive a vehicle, if not, they do not have a vehicle yet
     fetch("https://www.swapi.tech/api/vehicles?page=1&limit=100")
     .then(response => response.json())
     .then(data => console.log(data))
